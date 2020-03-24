Fix reassembly, fixed missing normalization in prediction

scripts/predict_organelles.py
@@ -59,16 +59,17 @@ def main():
         tomo = tomo[z_idx]
 
     # Normalization
-    # mean = tomo.mean()
-    # std = tomo.std()
+    if config["normalize"]:
+        mean = tomo.mean()
+        std = tomo.std()
 
-    # tomo -= mean
-    # tomo /= std
+        tomo -= mean
+        tomo /= std
 
     # Slice & predict
     tomo_patches = np.expand_dims(into_patches_3d(tomo, config["patch_size"], config["patch_dim"]), -1) # Add channel dim
     tomo_pred = model.predict(tomo_patches)
-    rec = from_patches_3d(tomo_pred[...,0], (config["patch_size"], config["patch_dim"]), tomo.shape, pad=config["crop"])
+    rec = from_patches_3d(tomo_pred[...,0], config["patch_dim"], tomo.shape, pad=config["crop"])
 
     if config["compensate_crop"]:
         padding = [((i - o)//2,)*2 for i, o in zip(orig_shape, rec.shape)]